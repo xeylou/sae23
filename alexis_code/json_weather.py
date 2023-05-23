da8c748ee0fe24e6be64a95501f440d13bc35378
xeylou <xeylou.pro@gmail.com>
2023-05-23 08:39:59 +0200

    magnifiying it

--- alexis_code/json_weather.py
+++ alexis_code/json_weather.py
@@ -9,7 +9,7 @@ lon=-0.5092243304975015
 ##getting wanted url by putting lat & long by get method
 url="https://api.met.no/weatherapi/locationforecast/2.0/compact?lat="+str(lat)+"&lon="+str(lon)
 
-##creating http header manually
+##creating http header manually for requets library
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0',
            'Cache-Control': 'no-cache, no-store, must-revalidate', 'Pragma': 'no-cache', 'Expires': '0'}
 
@@ -29,5 +29,13 @@ units = data["properties"]["meta"]["units"]
 weather = data["properties"]["timeseries"][0]["data"]["instant"]["details"]
 
 ##printing all data with associate units
+
 for key in weather.keys():
     print("%s = %2.2f %s" %(key, weather[key], units[key]))
+print("\n")
+
+##adding the output to a variable
+json_data=""
+for key in weather.keys():
+    json_data+=("%s = %2.2f %s" %(key, weather[key], units[key])+"\n")
+print(json_data)
