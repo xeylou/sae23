f60ab952f8b0ccc09915e395ed6c11f930bd022d
xeylou <xeylou.pro@gmail.com>
2023-05-25 10:59:37 +0200

    blup

--- alexis_code/php/files/index.php
+++ alexis_code/php/files/index.php
@@ -1,12 +1,11 @@
 <?php
 $db=new SQLite3('sae23.sqlite');
-// $querry0='SELECT * from announce WHERE announceId = 2';
-// $results0=$db->query($querry0);
-// $data0=$results0->fetchArray();
-// echo '<h2>'.$data0[1].'</h2>';
+$querry='SELECT * from weather';
+$results=$db->query($querry);
+while ($data=$results->fetchArray()){
+    echo "$data[0] . \n";
 
-
-// $myPDO = new PDO('sqlite:/home/xeylou/Downloads/iut/sae23/alexis_code/sqlite3/sae23.db');
+}
 
 
 ?>
--- alexis_code/python/weather_data_to_db.py
+++ alexis_code/python/weather_data_to_db.py
@@ -18,8 +18,8 @@ except:
 try:
     ##connection to the db & creating a cursor
     #db_name="alexis_code/sae23.sqlite"
-    db_name="php/files/sae23.sqlite"
-    connection=sqlite3.connect(db_name)
+    db_path="../php/files/sae23.sqlite"
+    connection=sqlite3.connect(db_path)
     cursor=connection.cursor()
 
     cursor.execute(
