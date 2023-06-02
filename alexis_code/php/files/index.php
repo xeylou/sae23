4fd979f74799f3ee45515d2bdd1d1786b2e019ba
xeylou <xeylou.pro@gmail.com>
2023-06-02 10:57:05 +0200

    ébauche php

--- alexis_code/php/files/index.php
+++ alexis_code/php/files/index.php
@@ -1,11 +1,31 @@
 <?php
 $db=new SQLite3('sae23.sqlite');
-$querry='SELECT * from weather';
+$querry='SELECT * from weather ORDER BY date DESC LIMIT 10';
 $results=$db->query($querry);
-while ($data=$results->fetchArray()){
-    echo "$data[0] . \n";
-
+echo'<h1>OpenWeather Data: (Python, PHP, MQTT, SQlite...) | SAÉ 23</h1>';
+echo'<h3>Les données récupérées seront présentées ci-dessous sous forme de tableau puis sous forme de graphique.</h3>';
+echo'<table>';
+echo'<tr>
+<th scope="col">Date & heure</th>
+<th scope="col">Latitude</th>
+<th scope="col">Longitude</th>
+<th scope="col">Température</th>
+<th scope="col">Revouvrement des nuages</th>
+<th scope="col">Humidité</th>
+<th scope="col">Vent de direction</th>
+<th scope="col">Vitesse du vent</th>
+</tr>';
+while($data=$results->fetchArray()){
+    echo'<tr>';
+    $columnCount=count($data);
+    for ($i=0; $i<$columnCount-8; $i++) {
+        echo'<td style="text-align: center">'.$data[$i].'</td>';
+    }
+    echo'</tr>';
 }
+echo'</table>';
 
 
+echo'<img src="tab1.png" title="Test"/>';
+echo'<img src="tab2.png" title="Test"/>';
 ?>
